480ad459b08f14982ddda1320b4837f456e880bd
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-05-12 10:57:45 +0200

    add EditTable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
-import Table from './components/features/Table';
+import TableForm from './components/features/TableForm';
 import NotFound from './components/pages/NotFound';
 import Header from './components/views/Header';
 import Footer from './components/views/Footer';
@@ -8,20 +8,24 @@ import { Container } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchTable } from './redux/tablesRedux';
+import { fetchTablesStatus } from './redux/tablesStatusRedux';
+import EditTable from './components/features/EditTable';
 
 const App = () => {
 
   const dispatch = useDispatch();
 
   useEffect(() => dispatch(fetchTable()), [dispatch])
-  
+  useEffect(() => dispatch(fetchTablesStatus()), [dispatch])
+
   return (
     <div>
       <Container>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/table/:id" element={<Table />} />
+          <Route path="/tableform" element={<TableForm />} />
+          <Route path="/edittable/:id" element={<EditTable />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
